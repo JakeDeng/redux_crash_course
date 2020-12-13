3e9c88aea96b28d036549e2b320aeab0faff1817
Jake Deng <jakedeng16@gmail.com>
2020-12-13 23:52:33 +0800

    basic post compoenet

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 function Postform() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    function onSubmit(e) {
+        e.preventDefault();
+        
+        const post = {
+            title: title,
+            body: body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST' ,
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
+    };
+
     return (
         <div>
             <h1>Add Post</h1>
+            <form onSubmit = {onSubmit}>
+                <div>
+                    <label> Title:</label><br/>
+                    <input type = "text" name = "title" onChange = {e => setTitle(e.target.value)} value = {title} />
+                </div>
+                <br/>
+                <div>
+                    <label>Body:</label><br/>
+                    <textarea name = "body" onChange = {e => setBody(e.target.value)} value = {body}/>
+                </div>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
             
         </div>
     )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,30 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Posts() {
+    //state management
+    const [posts, setPosts] = useState([]);
+
+    //run at the beginning
     useEffect(() => {
         async function fetchData() {
            let data = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
-           console.log(data);
+           setPosts(data);
            return data;
         }
         fetchData();
     }, []);
 
+    console.log(posts);
+
     return (
         <div>
             <h1>Posts</h1>
-            
+                {posts.map( post => {
+                    return <div key={post.id} >
+                                <h3>{post.title}</h3>
+                                <p>{post.body}</p>
+                            </div>
+                })}    
         </div>
     )
 }
